8343be9e25902f8c3b7bd326e631fddab9baa650
Juandi Vallejo <juadival@espol.edu.ec>
2024-01-21 12:38:39 -0500

    other: flag component

---
+++ src/lib/components/Flag.svelte
@@ -0,0 +1,9 @@
+<script>
+    /**
+	 * @type {{ nationality: any; }}
+	 */
+     export let fighter;
+</script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler-flags.min.css">
+
+<span class="flag flag-country-{fighter.nationality}"/>
